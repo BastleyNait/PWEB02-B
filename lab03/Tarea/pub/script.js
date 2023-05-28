26f5eee28ab2acaaf10e96ae297ae0e62780a160
BastleyNait <schirinosne@unsa.edu.pe>
2023-05-28 14:29:14 -0500

    2

--- lab03/Tarea/pub/script.js
+++ lab03/Tarea/pub/script.js
@@ -6,10 +6,11 @@ fetch('/titulos')
       listaTitulos.innerHTML += `
         <div class='caja'>
           <li>${titulo}</li>
-          <button onclick="ver(${titulo})"></button>
+          <button onclick='ver("${titulo}")'>Ver</button>
           <button>Editar</button>
           <button>Eliminar</button>
         </div>
+        <div id="contenido de ${titulo}"></div>
       `
     });
   });
@@ -18,17 +19,7 @@ function ver(titulo) {
   fetch(`/ver/${titulo}`)
   .then(response => response.json())
   .then(data => {
-    const listaTitulos = document.getElementById('lista-titulos');
-    data.forEach(titulo => {
-      listaTitulos.innerHTML += `
-        <div class='caja'>
-          <li>${titulo}</li>
-          <button onclick="ver(${titulo})"></button>
-          <button>Editar</button>
-          <button>Eliminar</button>
-        </div>
-      `
-    });
+    document.getElementById(`contenido de ${titulo}`).innerHTML = data.contenido;
   });
 }
   

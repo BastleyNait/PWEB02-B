55260dba666cd00c774e1a0c6c339070ce6e7416
BastleyNait <schirinosne@unsa.edu.pe>
2023-06-03 00:14:10 -0500

    mejorando ejercicio2

--- lab04/Tarea-del-Ajedrez/Ejercicio2b.py
+++ lab04/Tarea-del-Ajedrez/Ejercicio2b.py
@@ -1,5 +1,5 @@
 from interpreter import draw
 from chessPictures import *
 #Knight inverted
-knightI = Picture(knight.verticalMirror())
+knightI = knight.verticalMirror()
 draw(knight.join(knight.negative()).up(knightI.negative().join(knightI)))

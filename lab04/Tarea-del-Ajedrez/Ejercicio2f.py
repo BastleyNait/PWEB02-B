ac3209c01f8722781c43109f1f43b25347f33c76
BastleyNait <schirinosne@unsa.edu.pe>
2023-06-03 00:27:05 -0500

    demasiado ezzzzz

--- lab04/Tarea-del-Ajedrez/Ejercicio2f.py
+++ lab04/Tarea-del-Ajedrez/Ejercicio2f.py
@@ -1,2 +1,8 @@
 from interpreter import draw
 from chessPictures import *
+squareN = square.negative().join(square)
+squareB = square.join(square.negative())
+
+blanco = squareB.join(squareB.join(squareB.join(squareB)))
+negro = squareN.join(squareN.join(squareN.join(squareN)))
+draw(blanco.up(negro).up(blanco.up(negro)))

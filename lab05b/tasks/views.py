74496006aa3c5a366828ad4ba0f9ec63cc149d0b
BastleyNait <schirinosne@unsa.edu.pe>
2023-06-19 21:06:09 -0500

    mejorando registro

--- lab05b/tasks/views.py
+++ lab05b/tasks/views.py
@@ -4,24 +4,35 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 # Create your views here.
 
+
 def casa(request):
     return render(request, 'main.html')
 
+
 def registro(request):
+
     if request.method == 'GET':
         print("enviando form")
+        return render(request, 'registro.html', {
+            'form': UserCreationForm
+        })
+
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user=User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse("usuario creado satisfactioriamente ")
+                return render(request, 'registro.html', {
+                    'form': UserCreationForm,
+                    'error': 'usuario creado con exito'
+                })
             except:
-                return HttpResponse("no se pudo crear el usuario")
-        print(request.POST)
-        print("recibiendo datos")    
-    return render(request, 'registro.html', {
-        'form': UserCreationForm
-    })
-
-
+                return render(request, 'registro.html', {
+                    'form': UserCreationForm,
+                    'error': 'hubo un error inesperado'
+                })
+        return render(request, 'registro.html', {
+            'form': UserCreationForm,
+            'error': 'las contraseñas no coiciden'
+        })

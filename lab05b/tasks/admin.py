fe0534a4225ea1bbbeb8e8dc0b83a63a889b05f4
BastleyNait <schirinosne@unsa.edu.pe>
2023-06-23 09:41:51 -0500

    organizando

--- lab05b/tasks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Tareas
-
-class TaskAdmin(admin.ModelAdmin):
-    readonly_fields = ("creacion", )
-
-admin.site.register(Tareas, TaskAdmin)
-
--- lab05b/tasks/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Tareas
-class TaskForm(ModelForm):
-    class Meta: 
-        model = Tareas
-        fields = ['titulo', 'descripcion', 'importante']
-        

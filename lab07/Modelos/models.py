ad373fd7112110a4010bacbd4cb0de58c2af7632
BastleyNait <schirinosne@unsa.edu.pe>
2023-07-13 17:52:11 -0500

    agregando str a modelos

--- lab07/Modelos/models.py
+++ lab07/Modelos/models.py
@@ -2,15 +2,31 @@ from django.db import models
 
 # Create your models here.
 
-class Simple(models .Mode1):
-    text = models.CharFie1d(max_1ength=10)
-    number = models.IntegerFie1d(nu11=True)
+class Simple(models.Model):
+    text = models.CharField(max_length=10)
+    number = models.IntegerField(null=True)
     url = models.URLField(default='www.example.com')
     def __str__ (self) :
         return self.url
     
-class DateExamp1e(mode1s.Mode1):
-    the_date = models.DateTimeFie1d()
-    
-class NullExamp1e(models.Mode1):
+class DateExamp1e(models.Model):
+    the_date = models.DateTimeField()
+
+class NullExamp1e(models.Model):
     col = models.CharField(max_length=10, blank=True, null=True)
+
+"""relacion de uno a muchos en ete caso el que va ser
+relacionado con varios es el modelo lenguaje"""
+
+class Lenguage(models.Model):
+    name = models.CharField(max_length=10)
+    def __str__(self):
+        return self.name
+
+class Framework(models.Model):
+    name = models.CharField(max_length=10)
+    lenguage = models.ForeignKey(Lenguage, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name 
+
+
